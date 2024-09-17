c4f66d5550b91b1f3e6534aee8dc7db5ebd36d66
Kyoduke <caioduque.dev@gmail.com>
2024-09-17 10:29:20 -0300

    fix is_empty

--- stack/main.py
+++ stack/main.py
@@ -6,14 +6,14 @@ stack2 = Stack()
 stack1.push(1)
 stack1.push(2)  # [1, 2]
 
-print(f'Peek: {stack1.peek()} ')
+print(f"Peek: {stack1.peek()} ")
 
 stack1.push(3)
 stack1.push(4)
 stack1.push(5)
-print(f'Pilha original {stack1.items}')
+print(f"Pilha original {stack1.items}")
 
-while not stack1.isEmpty():
+while not stack1.is_empty():
     stack2.push(stack1.pop())
 
-print(f'Pilha invertida {stack2.items}')
+print(f"Pilha invertida {stack2.items}")
